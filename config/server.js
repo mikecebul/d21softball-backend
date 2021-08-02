2f23b389f42df3474c183af555818f6c1a29cc35
mikecebul <mikecebul@gmail.com>
2021-08-02 06:38:51 +0000

    backend live

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'http://api.mikecebul.cloud',
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b87a7dedf2150ac2ac7cb41b7ce48ad1'),
+      secret: env('ADMIN_JWT_SECRET', ''),
     },
   },
 });
