bd845e7555afdb907296ba51931af7372ca35c87
mikecebul <mikecebul@gmail.com>
2022-01-13 23:45:09 +0000

    Logout file

--- api/custom/controllers/Custom.js
+++ api/custom/controllers/Custom.js
@@ -2,18 +2,12 @@
 
 module.exports = {
   async logout(ctx) {
-<<<<<<< HEAD
-    ctx.cookies.set('token', "", {
-      domain: process.env.NODE_ENV === "development" ? "localhost" : "d21softball.org"
-    })
-=======
     ctx.cookies.set("token", "", {
       domain:
         process.env.NODE_ENV === "development"
           ? "localhost"
           : "d21softball.org",
     });
->>>>>>> cc43141e538031b27601dad53ebafe348eb533b8
     ctx.send({
       authorized: true,
       message: "Successfully destroyed session",
