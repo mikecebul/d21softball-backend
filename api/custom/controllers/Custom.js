fe93a8924e122072cafe86bb2e66fde3b73a6d34
mikecebul <mikecebul@gmail.com>
2022-01-13 18:34:32 -0500

    Add proper domain to custom logout controller

--- api/custom/controllers/Custom.js
+++ api/custom/controllers/Custom.js
@@ -2,9 +2,12 @@
 
 module.exports = {
   async logout(ctx) {
-    ctx.cookies.set('token', "", {
-      domain: process.env.NODE_ENV === "development" ? "localhost" : "mikecebul.cloud"
-    })
+    ctx.cookies.set("token", "", {
+      domain:
+        process.env.NODE_ENV === "development"
+          ? "localhost"
+          : "d21softball.org",
+    });
     ctx.send({
       authorized: true,
       message: "Successfully destroyed session",
